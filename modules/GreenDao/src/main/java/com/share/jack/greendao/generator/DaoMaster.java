d1ebe64a60fe71670a3894366bf88c4787112e0c
jack <17764576259@163.com>
2017-06-09 16:06:40 +0800

    删除.iml文件

--- modules/GreenDao/src/main/java/com/share/jack/greendao/generator/DaoMaster.java
+++ modules/GreenDao/src/main/java/com/share/jack/greendao/generator/DaoMaster.java
@@ -21,13 +21,11 @@ public class DaoMaster extends AbstractDaoMaster {
 
     /** Creates underlying database table using DAOs. */
     public static void createAllTables(Database db, boolean ifNotExists) {
-        ArticleInfoDao.createTable(db, ifNotExists);
         UserInfoDao.createTable(db, ifNotExists);
     }
 
     /** Drops underlying database table using DAOs. */
     public static void dropAllTables(Database db, boolean ifExists) {
-        ArticleInfoDao.dropTable(db, ifExists);
         UserInfoDao.dropTable(db, ifExists);
     }
 
@@ -47,7 +45,6 @@ public class DaoMaster extends AbstractDaoMaster {
 
     public DaoMaster(Database db) {
         super(db, SCHEMA_VERSION);
-        registerDaoClass(ArticleInfoDao.class);
         registerDaoClass(UserInfoDao.class);
     }
 
